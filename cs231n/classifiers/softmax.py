7de9bb1ca3ca153ce2488a92084459aa6f9cfa3a
Amira Esawy <amiram.esawy@gmail.com>
2020-04-07 19:20:05 +0200

    softmax_classifier

--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -24,6 +24,8 @@ def softmax_loss_naive(W, X, y, reg):
     # Initialize the loss and gradient to zero.
     loss = 0.0
     dW = np.zeros_like(W)
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
 
     #############################################################################
     # TODO: Compute the softmax loss and its gradient using explicit loops.     #
@@ -32,10 +34,30 @@ def softmax_loss_naive(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    for i in range(num_train):
+        s = X[i].dot(W)
+        s -= np.max(s) #to improve the numerical stability of the computation
+        s_yi = np.exp(s[y[i]])
+        s_sum = np.sum(np.exp(s))
+        loss += -np.log(s_yi / s_sum)
+       
+        for j in range(num_classes):
+            softmax_output_j = np.exp(s[j])/sum(np.exp(s))
+            if j == y[i]:
+                dW[:,j] += (-1 + softmax_output_j) *X[i]
+            else:
+                dW[:,j] += softmax_output_j *X[i]
+                
+    
 
     pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    loss = loss / num_train
+    loss += reg * np.sum (W * W)
+    dW = dW / num_train
+    dW += reg* W 
+
 
     return loss, dW
 
@@ -49,6 +71,9 @@ def softmax_loss_vectorized(W, X, y, reg):
     # Initialize the loss and gradient to zero.
     loss = 0.0
     dW = np.zeros_like(W)
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
+
 
     #############################################################################
     # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
@@ -57,7 +82,22 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+    s = X.dot(W)
+    s = s - np.max(s,axis=1, keepdims =True)
+    s_sum = np.sum(np.exp(s), axis=1, keepdims =True)
+    softmax = np.exp(s) / s_sum
+    dim = np.arange(num_train), y
+    loss = np.sum(-np.log(softmax[dim]))
+    
+    #to calculate the gradient
+    softmax[dim] -= 1
+    dW = X.T.dot(softmax)
+    
+    loss = loss / num_train
+    loss += reg * np.sum (W * W)
+    dW = dW / num_train
+    dW += reg* W 
+    
     pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
